b9347fb5d9e5e5726e35341e9076af11901ca200
LiqunHu <huliquns@126.com>
2024-06-07 22:44:59 +0800

    debug

--- tests/functional.rs
+++ tests/functional.rs
@@ -6,9 +6,9 @@ use {
     solana_program_test::*,
     solana_sdk::{
         account::Account, keccak, secp256k1_recover::secp256k1_recover, signature::Signer,
-        signer::keypair::Keypair, system_program, transaction::Transaction,
+        signer::keypair::Keypair, system_program, transaction::Transaction,pubkey::Pubkey
     },
-    example::state::{Attest, AttestationRequest, ExampleDataV1, Task},
+    example::{process_instruction, processor::ExampleInstruction, state::{AttestationRequest, ExampleDataV1}},
 };
 
 #[tokio::test]
@@ -20,12 +20,6 @@ async fn test() {
     let mut program_test = ProgramTest::new("example", program_id, processor!(process_instruction));
     let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
 
-    let task = Task {
-        task: "859b2b19b1da468ba15090960066e65d".to_string(),
-        schema: "c7eab8b7d7e44b05b41b613fe548edf5".to_string(),
-        notary: "e504ad91fbaad88362941a65b1c4c1e1cdd5cf69e27a3a08c8f51145c2e12c6a".to_string(),
-    };
-
     let mut a_signature = 
         hex::decode("e7f484adcaf1c8f53227901eaeed1f82cc49cfec5c36aefd31f9c6018ff56154359274fa0e33d0de3f9b9921e3c6c33f17c82e4431be572f0bfcd4cd65a31af101".to_string()).unwrap();
     let a_recovery_id = &a_signature[a_signature.len()-1].clone();
@@ -33,19 +27,20 @@ async fn test() {
 
     let mut n_signature = 
         hex::decode("7c8ba261642fb3d4f4cb198071346ffcaeb8b7587f81a055fb0796e84c1cf0b5726fd1b7a88c2a7d13b8c75bca49e15b1ed51f80cee7bbf600c102e5ab20384600".to_string()).unwrap();
-        let n_recovery_id = &n_signature[n_signature.len()-1].clone();
-        n_signature.pop();
+    let n_recovery_id = &n_signature[n_signature.len()-1].clone();
+    n_signature.pop();
 
     let mut attest = AttestationRequest {
-        task: task.task.clone(),
-        schema: task.schema.clone(),
+        task: "859b2b19b1da468ba15090960066e65d".to_string(),
+        schema: "c7eab8b7d7e44b05b41b613fe548edf5".to_string(),
         nullifier: "0xa3a5c8c3dd7dfe4abc91433fb9ad3de08344578713070983c905123b7ea91dda".to_string(),
         recipient: "A9Jk4bAebu5FNY3EvFF6Q6f86Sg38PE5fmVJbRugDpdf".to_string(),
-        public_fields_hash: "9dNsQaHyWg9c4jURSdvP9RxfEtBVN7Xh4sypL3kMH81B".to_string(),
-        a_recovery_id: a_recovery_id,
-        a_signature: a_signature,
-        n_recovery_id: 0,
-        n_signature: n_signature
+        public_fields_hash: "89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6".to_string(),
+        a_recovery_id: *a_recovery_id,
+        a_signature: a_signature.try_into().unwrap(),
+        n_recovery_id: *n_recovery_id,
+        n_signature: n_signature.try_into().unwrap(),
+        notary: "e504ad91fbaad88362941a65b1c4c1e1cdd5cf69e27a3a08c8f51145c2e12c6a".to_string()
     };
 
     let transaction = Transaction::new_signed_with_payer(
@@ -62,7 +57,7 @@ async fn test() {
         &[&payer],
         recent_blockhash,
     );
-    let res = banks_client
+    banks_client
         .process_transaction_with_metadata(transaction)
         .await
         .unwrap();
